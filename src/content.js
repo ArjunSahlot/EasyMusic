f8e37fcd144b3a63e7db362692681068c9506a93
ArjunSahlot <iarjun.sahlot@gmail.com>
2024-07-30 01:40:40 -0700

    minor tweaks

--- src/content.js
+++ src/content.js
@@ -1,13 +1,17 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.type === 'videoUrl') {
-    // create a floating video player that plays the video
+    for (let iframe of document.querySelectorAll('iframe.ezframe')) {
+      iframe.remove();
+    }
+
     let iframe = document.createElement('iframe');
     iframe.src = message.url;
+    iframe.class = "ezframe"
     iframe.style.position = 'absolute';
     iframe.style.top = '0';
     iframe.style.right = '0';
     iframe.style.width = '500px';
-    iframe.style.height = '50px';
+    iframe.style.height = '120px';
     iframe.style.border = 'none';
     iframe.style.zIndex = '9999';
     document.body.appendChild(iframe);
